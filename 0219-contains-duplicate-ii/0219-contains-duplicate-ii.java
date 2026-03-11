a1a43d3d42e891dcd81f977f79c5f7b289e88b6c
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-03-11 11:44:17 +0530

    Time: 30 ms (13.19%), Space: 90.3 MB (49.88%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer , Integer> seen = new HashMap<>();
+        for (int i = 0 ; i < nums.length ; i++){
+            if (seen.containsKey(nums[i]) && i - seen.get(nums[i]) <=k){
+                return true ;
+            }
+            seen.put(nums[i] , i);
+        }
+        return false ;
+
+    }
+}
