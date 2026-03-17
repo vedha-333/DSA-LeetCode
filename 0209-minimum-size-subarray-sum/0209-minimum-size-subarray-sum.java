3c9b094cbb2156093d1fcc22f63722ed5647964c
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-03-17 15:24:41 +0530

    Time: 1 ms (99.83%), Space: 62.1 MB (47.22%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,21 +1,34 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
+        //LEFT POINTER OF THE SLIDING WINDOW 
         int left = 0 ;
+
+        // STRORE THE SUM OF THE CURRENT WINDOW 
         int sum = 0 ;
-        int minCount = Integer.MAX_VALUE;
 
+        //I HAVE GIVEN THE VALUE FOR minLength is Integer.MAX_VALUE BECAUSE IT SHOULD STORE MIN LENGTH OF THE SLIDING WINDOW 
+        int minLength = Integer.MAX_VALUE ;
+
+        // RIGHT POINTER EXPANDS THE WINDOW 
         for (int right = 0 ; right < nums.length ; right++){
-            sum += nums[right]; // window expand right side 
+            // ADD CURRENT ELEMENT TO THE WINDOW 
+            sum += nums[right];
 
+           // IF SUM OF THE ELEMENT OF CURRENT ELEMENT IS MORE OR EQUAL TO THE TARGET THEN IT WILL EXECUTE THE WHILE LOOP 
+           // TRY TO SKRINK THE WINDOW TO GET THE MINLENGTH 
             while (sum >= target){
-                minCount = Math.min(minCount , right - left +1 );// the subarray size // min 
-                sum -= nums[left]; // remove from the sum the left element 
-                left++; // srink the left
+
+                // UPDATE THE MIN LENGTH OF THE MINUMIN SUBARRAY 
+                //RIGHT - LEFT +1 GIVE THE CURRENT WINDOWS SIZE
+                minLength = Math.min(minLength , right - left +1);
+
+                // REMOVE THE LEFTMOST ELEMENT FROM SUM 
+                sum -= nums[left];
+
+                // MOVE THE LEFT POINTER FROWARD TO SKRINK THE WINDOW 
+                left++;
             }
-
-
         }
-                    return minCount == Integer.MAX_VALUE ? 0 : minCount ;
-
+        return (minLength == Integer.MAX_VALUE)? 0 : minLength ;
     }
 }
