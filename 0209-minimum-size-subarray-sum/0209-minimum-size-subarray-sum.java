7b107d1e05a14eb32f653782370f607710cf474f
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-03-18 12:25:48 +0530

    Time: 1 ms (99.84%), Space: 62.1 MB (67.2%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,31 +1,16 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        //LEFT POINTER OF THE SLIDING WINDOW 
+        
         int left = 0 ;
+        int sum = 0 ; 
+        int minLength = Integer.MAX_VALUE;
 
-        // STRORE THE SUM OF THE CURRENT WINDOW 
-        int sum = 0 ;
-
-        //I HAVE GIVEN THE VALUE FOR minLength is Integer.MAX_VALUE BECAUSE IT SHOULD STORE MIN LENGTH OF THE SLIDING WINDOW 
-        int minLength = Integer.MAX_VALUE ;
-
-        // RIGHT POINTER EXPANDS THE WINDOW 
         for (int right = 0 ; right < nums.length ; right++){
-            // ADD CURRENT ELEMENT TO THE WINDOW 
             sum += nums[right];
 
-           // IF SUM OF THE ELEMENT OF CURRENT ELEMENT IS MORE OR EQUAL TO THE TARGET THEN IT WILL EXECUTE THE WHILE LOOP 
-           // TRY TO SKRINK THE WINDOW TO GET THE MINLENGTH 
             while (sum >= target){
-
-                // UPDATE THE MIN LENGTH OF THE MINUMIN SUBARRAY 
-                //RIGHT - LEFT +1 GIVE THE CURRENT WINDOWS SIZE
                 minLength = Math.min(minLength , right - left +1);
-
-                // REMOVE THE LEFTMOST ELEMENT FROM SUM 
                 sum -= nums[left];
-
-                // MOVE THE LEFT POINTER FROWARD TO SKRINK THE WINDOW 
                 left++;
             }
         }
