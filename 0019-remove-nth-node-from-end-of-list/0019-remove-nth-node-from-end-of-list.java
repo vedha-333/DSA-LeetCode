76b7c7d478a1da30ce9b40a28bcd68be9c4c5e0f
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-03-19 23:29:05 +0530

    Time: 0 ms (100%), Space: 43.7 MB (15.55%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,23 +10,31 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int length = 0 ;
-        ListNode temp = head ;
+        int length = 0 ; // length initilazate
+        ListNode temp = head ; //temp is the head
 
+        //count the length 
         while (temp != null){
             length++;
             temp = temp.next;
         }
 
+        //length is equal then remove the first node and return all node 
         if (length == n){
             return head.next;
         }
-
+        
+        //Find the position 
         int position = length - n ;
+
+        // current node is head to remove the postion next node 
         ListNode current = head;
         for (int i = 1 ; i < position ; i++){
             current = current.next;
         }
+
+        //current which was pointing to the next node it will replace the next node pointing 
+        // if that next node is not pointing to anyone then it collected by garbage collector 
         current.next = current.next.next;
 
          return head ;
