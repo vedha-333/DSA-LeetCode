b7a3fdeed8a87f90296a4482cf88736e48a8f230
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-03-20 20:04:29 +0530

    Time: 6 ms (87.82%), Space: 44.2 MB (60.09%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,46 +1,40 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        //if s1 is bigger , no permutation possible
-        if (s1.length() > s2.length()) return false;
+        if (s1.length() > s2.length() ) return false;
 
-        // frequency array for s1 and current window 
         int[] s1Count = new int[26];
-        int[] windowCount = new int[26];
+        int[] s2Count = new int[26];
 
-        //add character to s1Count
         for (char c : s1.toCharArray()){
             s1Count[c - 'a']++;
-        }
-        
-        //left pointer for window
+        } 
+
         int left = 0 ;
 
-        //sliding window over s2
         for (int right = 0 ; right < s2.length() ; right++){
-            // counting elements of current window 
-            windowCount[s2.charAt(right) - 'a']++;
+            s2Count[s2.charAt(right) - 'a']++;
 
-            //If window size exceeds s1 length, skink it 
-            if (right - left +1 > s1.length()){
-                windowCount[s2.charAt(left) - 'a']--;
+            if (right - left + 1 > s1.length()){
+                s2Count[s2.charAt(left) - 'a']--;
                 left++;
             }
-            
-            //check if window matches s1 frequency 
-            if (matches(windowCount , s1Count)){
+
+            if (matches(s1Count , s2Count)){
                 return true ;
             }
         }
-        return false ;
-    }
-    //compare two frequency arrays
-private boolean matches(int[] a , int[] b){
-    for (int i =0 ; i < 26 ; i++){
-        if (a[i] != b[i]){
-            return false;
-            }
-    }
-return true;
-}
-}
+        return false;
 
+    }
+
+
+private boolean matches(int[] a , int[] b ){
+    for (int i = 0 ; i < 26 ; i++){
+        if(a[i] != b[i]){
+            return false ;
+        }
+    }
+     return true ;
+
+}
+}
