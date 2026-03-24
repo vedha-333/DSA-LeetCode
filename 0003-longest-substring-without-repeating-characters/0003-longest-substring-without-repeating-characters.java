514db231157f77d3bdee9a7ec1ea1986cdfe8ab6
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-03-24 20:16:48 +0530

    Time: 6 ms (69.46%), Space: 46.4 MB (44.85%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,21 +1,22 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int left = 0 ;
-        int maxlength = 0 ;
+       int maxLength = 0 ;
+       int left = 0 ;
 
-        HashSet<Character> seen = new HashSet<>();
+       HashSet<Character> seen = new HashSet<>();
 
-        for (int right = 0 ; right < s.length() ; right++ ){
-            char currentChar = s.charAt(right);
+       for (int right = 0 ; right < s.length() ; right++){
+        char currentChar = s.charAt(right);
 
-            while (seen.contains(currentChar)){
-                seen.remove(s.charAt(left));
-                left++;
-            }
-            seen.add(currentChar);
-            maxlength = Math.max(maxlength ,right-left +1);
+        while (seen.contains(currentChar)){
+            seen.remove(s.charAt(left));
+            left++;
         }
-        return maxlength;
 
+        seen.add(currentChar);
+        maxLength = Math.max(maxLength , right - left +1 );
+
+       }
+       return maxLength; 
     }
 }
