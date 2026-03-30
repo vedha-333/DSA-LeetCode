6f7314e1fe01e5b3c4af72e6ed9c19b12af120a9
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-03-30 11:13:34 +0530

    Time: 0 ms (100%), Space: 42.5 MB (84.08%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class MyQueue {
+
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+
+    // Constructor
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    // Push element to back of queue
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    // Remove element from front of queue
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    // Get front element
+    public int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    // Check if queue is empty
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
