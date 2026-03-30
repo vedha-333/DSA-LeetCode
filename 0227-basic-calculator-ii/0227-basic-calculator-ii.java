ce19cab111de38cbf4949d38d31e6f51ae7f3a01
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-03-30 14:46:55 +0530

    Time: 15 ms (82.64%), Space: 48.9 MB (13.8%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,25 +1,28 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> st = new Stack<>();
-        int num = 0;
+        Stack<Integer> stack = new Stack<>();
+        int num = 0 ;
         char sign = '+';
 
-        for (char c : (s + "+").toCharArray()) {
-            if (Character.isDigit(c)) num = num * 10 + c - '0';
+        for (char current : (s +'+').toCharArray()){
+            if (Character.isDigit(current)){
+                num = num * 10 + current - '0';
+            } else if (current  != ' '){
+                if (sign == '+') stack.push(num);
+                else if (sign == '-') stack.push(-num);
+                else if (sign == '*') stack.push(stack.pop() * num);
+                else if (sign == '/') stack.push(stack.pop() / num);
 
-            else if (c != ' ') {
-                if (sign == '+') st.push(num);
-                else if (sign == '-') st.push(-num);
-                else if (sign == '*') st.push(st.pop() * num);
-                else st.push(st.pop() / num);
-
-                sign = c;
-                num = 0;
+                sign = current ;
+                num = 0 ;
             }
-        }
 
-        int res = 0;
-        for (int x : st) res += x;
-        return res;
+        }
+        int res = 0 ;
+        for (int x : stack){
+            res += x;
+        }
+    return res ;
+
     }
 }
