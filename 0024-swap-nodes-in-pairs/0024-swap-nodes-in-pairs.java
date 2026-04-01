ad1e5ef8d3c104bd84d040e85af1dc9c1bbe97a1
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-04-01 11:15:07 +0530

    Time: 0 ms (100%), Space: 42.9 MB (75.79%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -10,9 +10,10 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        if (head == null || head.next == null) return head ;
+        if (head == null || head.next == null){
+            return head;
+        }
         ListNode nextNode = head.next;
-
         head.next = swapPairs(nextNode.next);
 
         nextNode.next = head ;
