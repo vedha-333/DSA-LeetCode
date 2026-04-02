9a7e200acddf9e9dd3bc689fa1afaa83c60f7827
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-04-02 09:44:05 +0530

    Time: 3 ms (24.77%), Space: 42.4 MB (98.35%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -6,28 +6,24 @@ class Solution {
         String currentString = "";
         int currentNumber = 0;
 
-        for (char ch : s.toCharArray()) {
-
-            if (Character.isDigit(ch)) {
+        for (char ch : s.toCharArray()){
+            if (Character.isDigit(ch)){
                 currentNumber = currentNumber * 10 + (ch - '0');
-            } 
-            else if (ch == '[') {
+            }else if (ch == '['){
                 repeatStack.push(currentNumber);
                 stringStack.push(currentString);
 
-                currentString = "";
                 currentNumber = 0;
-            } 
-            else if (ch == ']') {
-                int repeatTimes = repeatStack.pop();
-                String previousString = stringStack.pop();
+                currentString = "";
+            }else if (ch == ']'){
+                int repeatNumber = repeatStack.pop();
+                String previous = stringStack.pop();
 
-                currentString = previousString + currentString.repeat(repeatTimes);
-            } 
-            else {
+                currentString = previous + currentString.repeat(repeatNumber);
+            }else {
                 currentString += ch;
             }
         }
-        return currentString;
+        return currentString ;
     }
 }
