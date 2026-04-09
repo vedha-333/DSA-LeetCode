d31690c5c3c3ad93a7f4fbf4dea837d33742ed13
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-04-09 18:14:18 +0530

    Time: 1 ms (77.24%), Space: 43.5 MB (92.27%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,33 +15,35 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result ;
+       List<List<Integer>> result = new ArrayList<>();
+       if (root == null) return result ;
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
+       Queue<TreeNode> queue = new LinkedList<>();
+       queue.offer(root);
 
-        boolean leftToRight = true ;
+       boolean leftToRight = true ;
 
-        while (!queue.isEmpty()){
-            int size = queue.size();
-            List<Integer> level = new ArrayList<>();
+       while(!queue.isEmpty()){
+        int size = queue.size();
+        List<Integer> level = new ArrayList<>();
 
-            for (int i= 0 ; i < size ; i++){
-                TreeNode node = queue.poll();
+        for (int i = 0 ; i < size ; i++){
+            TreeNode node = queue.poll();
 
-                if (leftToRight) {
-                    level.add(node.val);
-                }else {
-                    level.add(0 , node.val);
-                }
-
-                if (node.left != null) queue.offer(node.left);
-                if (node.right != null) queue.offer(node.right);
+            if (leftToRight) {
+                level.add(node.val);
+            }else{
+                level.add(0, node.val);
             }
-            result.add(level);
-            leftToRight = !leftToRight ;
+
+            if (node.left != null) queue.offer(node.left);
+            if (node.right != null) queue.offer(node.right);
+
         }
-        return result ;
+        result.add(level);
+        leftToRight = !leftToRight ;
+
+       }
+       return result ; 
     }
 }
