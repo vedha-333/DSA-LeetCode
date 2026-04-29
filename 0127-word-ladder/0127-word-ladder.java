301f41cc053db0b5d1ca9dcc9f2021f811bd15c3
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-04-29 23:40:56 +0530

    Time: 67 ms (80.78%), Space: 47.7 MB (79.63%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>(wordList);
+        if (!set.contains(endWord)) return 0 ;
+
+        Queue<String> q = new LinkedList<>();
+        q.offer(beginWord);
+
+        int level = 1 ;
+
+        while (!q.isEmpty()){
+            for (int s = q.size() ; s > 0 ; s--){
+                String word = q.poll();
+                if (word.equals(endWord)) return level ;
+                char[] arr = word.toCharArray();
+
+                for (int i = 0 ; i < arr.length ; i++){
+                    char org = arr[i];
+                    for (char ch = 'a' ; ch <= 'z' ; ch++){
+                        arr[i] = ch ;
+                        String newWord = new String(arr);
+                        if (set.contains(newWord)){
+                            q.offer(newWord);
+                            set.remove(newWord);
+                        }
+                    }
+                    arr[i] = org;
+                }
+            }
+            level++;
+        }
+        return 0 ;
+    }
+}
