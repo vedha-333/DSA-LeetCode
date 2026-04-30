2f03576932b0e57d2571a2a750332c536924dc51
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-05-01 00:31:07 +0530

    Time: 2 ms (99.82%), Space: 52.5 MB (22.7%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,39 +1,35 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int count = 0;
+        int count = 0 ; 
+        int m = grid.length ;
+        int n = grid[0].length ;
 
-        int m = grid.length;
-        int n = grid[0].length;
+        for (int i = 0 ; i < m ; i++){
+            for (int j = 0 ; j < n ; j++){
+                if (grid[i][j] == '1'){
+                    count++;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
-                if (grid[i][j] == '1') {
-                    count++;               // Found new island
-                    dfs(grid, i, j);       // Remove whole island
+                    dfs(grid , i , j);                
                 }
             }
         }
-
-        return count;
+        return count ;
     }
 
-    private void dfs(char[][] grid, int i, int j) {
-        int m = grid.length;
-        int n = grid[0].length;
+    private void dfs(char[][] grid,int i , int j){
+    int m = grid.length ;
+    int n = grid[0].length ;
 
-        // Boundary + water check
-        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0') {
-            return;
-        }
-
-        // Mark as visited
-        grid[i][j] = '0';
-
-        // Explore 4 directions
-        dfs(grid, i + 1, j); // down
-        dfs(grid, i - 1, j); // up
-        dfs(grid, i, j + 1); // right
-        dfs(grid, i, j - 1); // left
+    if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0'){
+        return;
     }
+
+    grid[i][j] = '0';
+
+    dfs(grid , i+1 , j);
+    dfs(grid , i-1 , j);
+    dfs(grid , i , j+1);
+    dfs(grid , i , j-1);
 }
+}
+
