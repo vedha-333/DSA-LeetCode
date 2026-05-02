8e44a17ca5b7d4b900412e277e143b828bd41395
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-05-02 16:21:36 +0530

    Time: 1 ms (100%), Space: 46.5 MB (49.78%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -14,30 +14,29 @@
  * }
  */
 class Solution {
-    TreeNode first = null;
-    TreeNode second = null;
-    TreeNode prev = null;
+    TreeNode first ;
+    TreeNode second ;
+    TreeNode prev ;
     public void recoverTree(TreeNode root) {
         inOrder(root);
+
         int temp = first.val ;
-        first.val = second.val ;
+        first.val = second.val;
         second.val = temp ;
     }
 
-    public void inOrder(TreeNode root){
-        if (root== null) return ;
+    private void inOrder(TreeNode root ){
+        if (root == null) return ;
         inOrder(root.left);
 
         if (prev != null && prev.val > root.val){
             if (first == null){
                 first = prev;
             }
-        second = root ;
+        second = root;
         }
 
         prev = root ;
         inOrder(root.right);
-
-
     }
 }
