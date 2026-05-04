dd41cd10e29926f8e0716a140ad8bacafa7b6183
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-05-04 16:15:23 +0530

    Time: 4 ms (99.81%), Space: 63.6 MB (27.54%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,49 +1,31 @@
 class Solution {
-
     public List<Integer> eventualSafeNodes(int[][] graph) {
-
         int n = graph.length;
-
-        // 0 = unvisited
-        // 1 = visiting
-        // 2 = safe
-        int[] color = new int[n];
+        int[] colour = new int[n];
 
         List<Integer> ans = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-
-            if (dfs(graph, color, i)) {
+        for (int i = 0  ; i< n ; i++){
+            if (dfs (graph , colour , i)){
                 ans.add(i);
             }
         }
-
-        return ans;
+        return ans ;
     }
 
-    private boolean dfs(int[][] graph, int[] color, int node) {
-
-        // already processed
-        if (color[node] != 0) {
-
-            // safe only if color == 2
-            return color[node] == 2;
+    public boolean dfs(int[][] graph , int[] colour , int node){
+        if (colour[node] != 0 ){
+            return colour[node] == 2 ;
         }
 
-        // mark visiting
-        color[node] = 1;
+        colour[node] = 1 ;
 
-        for (int nei : graph[node]) {
-
-            // if neighbor unsafe
-            if (!dfs(graph, color, nei)) {
-                return false;
-            }
+        for (int neighbour : graph[node]){
+            if (!dfs(graph , colour , neighbour)) return false ;
         }
 
-        // all neighbors safe
-        color[node] = 2;
+        colour[node] = 2;
 
-        return true;
+        return true ;
     }
 }
