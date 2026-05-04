ee5b78a5d29a1f17af3e693403526d6c95820a26
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-05-04 19:26:51 +0530

    Time: 26 ms (48.1%), Space: 44.2 MB (76.03%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -21,19 +21,17 @@ class Node {
 class Solution {
     private Map<Node , Node> map = new HashMap<>();
     public Node cloneGraph(Node node) {
-        if (node== null) return null ;
-
+        if (node == null) return null;
         if (map.containsKey(node)){
             return map.get(node);
         }
 
         Node clone = new Node(node.val);
-        map.put(node , clone);
+        map.put(node, clone);
 
-        for (Node neighbor : node.neighbors){
-            clone.neighbors.add(cloneGraph(neighbor));
+        for (Node neighbour : node.neighbors){
+            clone.neighbors.add(cloneGraph(neighbour));
         }
         return clone ;
-        
     }
 }
