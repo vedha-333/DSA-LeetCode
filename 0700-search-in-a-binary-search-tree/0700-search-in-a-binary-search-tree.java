e33e20b85acd303b2c5c0f33b53257749994a569
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-05-05 09:43:53 +0530

    Time: 0 ms (100%), Space: 47.2 MB (15.25%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -18,7 +18,7 @@ class Solution {
         if (root == null) return null ;
         if (root.val == val) return root ;
 
-        if (val < root.val ){
+        if (val < root.val){
             return searchBST(root.left , val );
         }else{
             return searchBST(root.right , val);
