6325f941db3e12427b60cd07a94b1c0b3b8a15d2
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-08-20 23:59:01 +0530

    Time: 2 ms (99.96%), Space: 52.7 MB (29.88%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,38 +1,43 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int candidate1 = 0 , candidate2 = 0 ;
-        int count1 = 0 , count2 = 0 ;
-        for (int ele : nums){
-            if (count1 > 0 && candidate1 == ele){
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+
+        // 1st Pass: Find the top 2 potential candidates
+        for (int num : nums) {
+            if (count1 > 0 && num == candidate1) {
                 count1++;
-            }else if (count2 > 0 && candidate2 == ele){
+            } else if (count2 > 0 && num == candidate2) {
                 count2++;
-            }else if (count1 == 0 ){
-                candidate1 = ele ;
+            } else if (count1 == 0) {
+                candidate1 = num;
                 count1 = 1;
-            }else if (count2 == 0){
-                candidate2 = ele;
-                count2 = 1 ;
-            }else {
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
                 count1--;
                 count2--;
             }
         }
 
-        count1 = 0 ;
-        count2 = 0 ;
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0 ; i < nums.length ; i++){
-            if (candidate1 != Integer.MIN_VALUE && candidate1 == nums[i]){ 
+        // 2nd Pass: Verify the actual frequencies of the candidates
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) {
                 count1++;
-            }else if(candidate2 != Integer.MIN_VALUE && candidate2 == nums[i] ){
+            } else if (num == candidate2) {
                 count2++;
             }
         }
-        int threshold = nums.length / 3 ;
+
+        List<Integer> list = new ArrayList<>();
+        int threshold = nums.length / 3;
+
         if (count1 > threshold) list.add(candidate1);
         if (count2 > threshold) list.add(candidate2);
-        
-        return list ;
+
+        return list;
     }
 }
