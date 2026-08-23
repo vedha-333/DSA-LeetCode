2374605eb6bd8d79c91d88128a13ca4bf46206fb
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-08-23 23:56:27 +0530

    Time: 2 ms (41.11%), Space: 46.9 MB (94.78%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] vis = new boolean[n];
+        int count = 0;
+        for(int i = 0; i<n; i++){
+            if(!vis[i]){
+                count++;
+                Queue <Integer> q = new ArrayDeque<>();
+                vis[i] = true;
+                q.offer(i);
+                while(!q.isEmpty()){
+                    int node = q.poll();
+                    for(int j = 0; j<n; j++){
+                        if(isConnected[node][j]==1 && !vis[j]){
+                            vis[j]= true;
+                            q.offer(j);
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
