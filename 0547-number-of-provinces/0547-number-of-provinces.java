7df3317929b6ddc74546501483c41df0486ce9cb
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-08-24 00:09:25 +0530

    Time: 2 ms (41.11%), Space: 46.9 MB (94.78%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -2,18 +2,19 @@ class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         boolean[] vis = new boolean[n];
-        int count = 0;
-        for(int i = 0; i<n; i++){
-            if(!vis[i]){
+        int count = 0 ;
+        
+        for (int i = 0 ; i < n ; i++){
+            if (!vis[i]){
                 count++;
-                Queue <Integer> q = new ArrayDeque<>();
-                vis[i] = true;
+                Queue<Integer> q = new ArrayDeque<>();
                 q.offer(i);
-                while(!q.isEmpty()){
+
+                while (!q.isEmpty()){
                     int node = q.poll();
-                    for(int j = 0; j<n; j++){
-                        if(isConnected[node][j]==1 && !vis[j]){
-                            vis[j]= true;
+                    for (int j =0 ; j < n ;j++){
+                        if (isConnected[node][j] == 1 && !vis[j]){
+                            vis[j] = true ;
                             q.offer(j);
                         }
                     }
