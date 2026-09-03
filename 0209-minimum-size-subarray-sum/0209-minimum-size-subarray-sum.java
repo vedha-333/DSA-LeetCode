3c0588e69fc6d4f30e9ea50bcd33f23fc4503e42
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-09-03 23:41:36 +0530

    Time: 1 ms (99.85%), Space: 69.1 MB (67%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,17 +1,19 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int left = 0 , minLength = Integer.MAX_VALUE , sum = 0;
+        int left = 0 , right = 0 ;
+        int sum =0 , minLength = Integer.MAX_VALUE ;
 
-        for (int right = 0 ; right < nums.length ; right++){
+        while (right < nums.length ){
             sum += nums[right];
-
-            while (sum >= target){
-                minLength = Math.min(minLength , right - left +1 );
+            while (sum >= target ){
                 sum -= nums[left];
+                minLength = Math.min(minLength , right - left + 1);
                 left++;
             }
 
+            right++;
         }
-        return (minLength == Integer.MAX_VALUE) ? 0 : minLength ;
+
+        return minLength == Integer.MAX_VALUE ? 0 : minLength  ;
     }
 }
