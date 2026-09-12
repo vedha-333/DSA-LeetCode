be27f73215ba385e308558577cca56dc8df81a16
Vedant Yadav Pandilwad <pandilwad.vedant.s70@kalvium.community>
2026-09-12 15:10:07 +0530

    Time: 14 ms (43.83%), Space: 53.3 MB (10.68%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,42 +1,16 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int candidate1 = 0, candidate2 = 0;
-        int count1 = 0, count2 = 0;
-
-        // 1st Pass: Find the top 2 potential candidates
-        for (int num : nums) {
-            if (count1 > 0 && num == candidate1) {
-                count1++;
-            } else if (count2 > 0 && num == candidate2) {
-                count2++;
-            } else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
-            }
-        }
-
-        // 2nd Pass: Verify the actual frequencies of the candidates
-        count1 = 0;
-        count2 = 0;
-        for (int num : nums) {
-            if (num == candidate1) {
-                count1++;
-            } else if (num == candidate2) {
-                count2++;
-            }
-        }
-
+        HashMap<Integer , Integer> map = new HashMap<>();
         List<Integer> list = new ArrayList<>();
-        int threshold = nums.length / 3;
+        for (int ele : nums){
+            map.put(ele , map.getOrDefault(ele , 0) +1 );
+        }  
 
-        if (count1 > threshold) list.add(candidate1);
-        if (count2 > threshold) list.add(candidate2);
+        for (int key : map.keySet()){
+            if (map.get(key) > nums.length / 3){
+                list.add(key) ;
+            }
+        }
 
         return list;
     }
